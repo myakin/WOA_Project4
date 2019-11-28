65c1c5b9446c3c62bbc19336a5cb08ee76df7c47
Murat Yakin <yakin_murat@yahoo.com>
2019-11-28 11:41:36 +0300

    Ders 10 ogle arasi

---
+++ Assets/0-Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float followDistance = 3f;
+    public float upOffset = 1.7f;
+    public float rightOffset = 0;
+    public float xRotation = -5f;
+    public float yRotation = 0;
+    public float dampeningFactor = 0.01f;
+    private Vector3 offset;
+
+    
+
+    private void LateUpdate() {
+        // kameram oyuncunun pozisyonunun followDistance kaar arkasina olacak
+        // kameramin poziyonu + (oyuncumun arka yonu * followDistance)
+        transform.position = target.position + (-target.forward * followDistance) + (transform.up * upOffset) + (transform.right * rightOffset);
+        transform.rotation = (target.rotation * Quaternion.Euler(xRotation, yRotation , 0));
+    }
+
+    public void SmoothMovemet(Vector3 aTargetPosition) {
+        offset += aTargetPosition;
+        Vector3 oldPos = transform.position;
+        Vector3 newPos = Vector3.LerpUnclamped(oldPos, aTargetPosition, dampeningFactor);
+        transform.position = newPos;
+        offset -= newPos - oldPos;
+    }
+}
---
+++ Assets/0-Scripts/Character/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float rotSpeed;
+    public float jumpMagnitude;
+    private float ver, hor, mouseY, mouseX, jump, fire1;
+
+    
+    private void Update() {
+        ver = Input.GetAxis("Vertical");
+        hor = Input.GetAxis("Horizontal");
+        mouseY = Input.GetAxis("Mouse Y");
+        mouseX = Input.GetAxis("Mouse X");
+        jump = Input.GetAxis("Jump");
+        fire1 = Input.GetAxis("Fire1");
+        
+        if (ver!=0) {
+            MoveForwardBack();
+        }
+        if (hor!=0) {
+            MoveLeftRight();
+        }
+        TurnLeftRight();
+
+    }
+
+
+    private void MoveForwardBack() {
+        transform.position += (transform.forward * (ver * moveSpeed));
+    }
+    private void MoveLeftRight() {
+        transform.position += (transform.right * (hor * moveSpeed));
+    }
+
+    private void TurnLeftRight() {
+        transform.rotation *= Quaternion.Euler(0, mouseX * rotSpeed, 0);
+    }
+
+    private void LookUpDown() {
+
+    }
+
+    private void Jump() {
+
+    }
+
+    private void Fire() {
+
+    }
+}
